b18ce36f37ee8ba87b16b5a491829529e69baf15
nodirbek75 <nodirbek7077@gmail.com>
2025-01-17 18:15:48 +0500

    set up redux-toolkit

--- App.tsx
+++ App.tsx
@@ -8,21 +8,23 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {ApolloProvider} from '@apollo/client';
+import {Provider} from 'react-redux';
 
+import {store} from './src/store';
 import Layout from './src/routes';
 import client from './src/graphql/ApolloClient';
 import {ActivityIndicatorProvider} from './src/contexts/ActivityIndicator';
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle={'light-content'} backgroundColor={'#fff'} />
       <ApolloProvider client={client}>
         <ActivityIndicatorProvider>
           <Layout />
         </ActivityIndicatorProvider>
       </ApolloProvider>
-    </>
+    </Provider>
   );
 }
 
