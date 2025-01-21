f74e6e9ad08df5158ee0e44092e9f45f3ed37efa
nodirbek75 <nodirbek7077@gmail.com>
2025-01-21 13:33:19 +0500

    update PrimaryButton and implement SecondaryButton

--- src/components/buttons/SecondaryButton.tsx
+++ src/components/buttons/SecondaryButton.tsx
@@ -1,34 +1,44 @@
 import React from 'react';
 import {ViewStyle} from 'react-native';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
 type Props = {
+  icon: string;
   btnText: string;
   btnStyle?: ViewStyle;
   onPress: () => void;
 };
 
-const PrimaryButton: React.FC<Props> = ({btnText, btnStyle, onPress}) => {
+const SecondaryButton: React.FC<Props> = ({
+  icon,
+  btnText,
+  btnStyle,
+  onPress,
+}) => {
   return (
     <Wrapper style={btnStyle} onPress={onPress} activeOpacity={0.5}>
+      <Icon name={icon} size={20} solid />
       <Text>{btnText}</Text>
     </Wrapper>
   );
 };
 
-export default PrimaryButton;
+export default SecondaryButton;
 
 const Wrapper = styled.TouchableOpacity`
-  width: 100%;
-  background-color: #061237;
+  width: 90%;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
-  padding-vertical: 15px;
+  border: 1px solid #061237;
+  padding-vertical: 10px;
 `;
 
 const Text = styled.Text`
   font-size: 20px;
   font-family: 'Outfit-Bold';
-  color: #fff;
+  color: #000;
+  margin-left: 5px;
 `;
