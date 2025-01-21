8203096571f0d447e5a0415e761a2f64fd337df2
nodirbek75 <nodirbek7077@gmail.com>
2025-01-21 13:44:29 +0500

    update Main screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,14 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // screens
 import {Auth, Main} from '../screens';
 
+// hooks
+import {useAppSelector} from '../store/hooks';
+
 const Stack = createNativeStackNavigator<RootStackType>();
 
 const Root = () => {
+  const {username} = useAppSelector(state => state.user);
+
   return (
     <Stack.Navigator
       initialRouteName="Auth"
@@ -17,7 +22,15 @@ const Root = () => {
         headerShadowVisible: false,
       }}>
       <Stack.Screen name="Auth" component={Auth} />
-      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{
+          headerShown: true,
+          headerTitle: `Pay ${username}`,
+          headerTitleStyle: {fontFamily: 'Outfit-Bold'},
+        }}
+      />
     </Stack.Navigator>
   );
 };
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,82 +1,41 @@
-import {useQuery} from '@apollo/client';
-import React, {useEffect} from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
-import {AccountDefaultWallets} from '../graphql/queries';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Dimensions} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome6';
 
-const width = Dimensions.get('screen').width;
+// components
+import {
+  Amount,
+  Note,
+  NumPad,
+  PrimaryButton,
+  SecondaryButton,
+} from '../components';
+
+// hooks
+import {useAppDispatch} from '../store/hooks';
+import {updateAmount} from '../store/slices/amountSlice';
 
 type Props = NativeStackScreenProps<RootStackType, 'Main'>;
 
 const Main: React.FC<Props> = ({navigation, route}) => {
-  const {loading, error, data} = useQuery(AccountDefaultWallets, {
-    variables: {username: route.params.username},
-  });
-
-  useEffect(() => {
-    console.log('>>>>>>>>>>', loading);
-    console.log('>>>>>>>>>>', error);
-    console.log('>>>>>>>>>>', data);
-  }, [loading, error, data]);
+  const dispatch = useAppDispatch();
 
   return (
     <Wrapper>
-      <RowWrapper>
-        <AmountWrapper>
-          <Primary>$ 200</Primary>
-          <Secondary>≈ 200000 sats</Secondary>
-        </AmountWrapper>
-      </RowWrapper>
-      <NumbersWrapper>
-        <RowWrapper>
-          <NumBtn>
-            <NumText>1</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>2</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>3</NumText>
-          </NumBtn>
-        </RowWrapper>
-        <RowWrapper>
-          <NumBtn>
-            <NumText>4</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>5</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>6</NumText>
-          </NumBtn>
-        </RowWrapper>
-        <RowWrapper>
-          <NumBtn>
-            <NumText>7</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>8</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>9</NumText>
-          </NumBtn>
-        </RowWrapper>
-        <RowWrapper>
-          <NumBtn>
-            <NumText>.</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>0</NumText>
-          </NumBtn>
-          <NumBtn>
-            <NumText>
-              <Icon name={'delete-left'} size={20} solid />
-            </NumText>
-          </NumBtn>
-        </RowWrapper>
-      </NumbersWrapper>
+      <BodyWrapper>
+        <Amount />
+        <Note />
+        <NumPad />
+      </BodyWrapper>
+      <BtnsWrapper>
+        <PrimaryButton btnText="Next" onPress={() => {}} />
+        <SecondaryButton
+          icon={'arrow-rotate-left'}
+          btnText="Clear"
+          btnStyle={{marginTop: 10}}
+          onPress={() => dispatch(updateAmount('clearInput'))}
+        />
+      </BtnsWrapper>
     </Wrapper>
   );
 };
@@ -86,42 +45,15 @@ export default Main;
 const Wrapper = styled.View`
   flex: 1;
   background-color: #fff;
+  padding-top: 10px;
+  padding-bottom: 30px;
   padding-horizontal: 20px;
 `;
 
-const RowWrapper = styled.View`
-  flex-direction: row;
+const BodyWrapper = styled.View`
+  flex: 1;
+`;
+
+const BtnsWrapper = styled.View`
   align-items: center;
-  justify-content: space-around;
-`;
-
-const AmountWrapper = styled.View`
-  align-items: center;
-  justify-content: center;
-`;
-
-const Primary = styled.Text`
-  font-size: 26px;
-  font-weight: bold;
-`;
-
-const Secondary = styled.Text`
-  font-size: 18px;
-`;
-
-const NumbersWrapper = styled.View``;
-
-const NumBtn = styled.TouchableOpacity`
-  width: ${width / 5}px;
-  height: ${width / 5}px;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #adadad;
-  border-radius: 10px;
-  margin-bottom: 10px;
-`;
-
-const NumText = styled.Text`
-  font-size: 40px;
-  font-weight: bold;
 `;
