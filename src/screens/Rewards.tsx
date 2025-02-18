8a5d28e3ec110dfa49a978f136b589f14ee95145
nodirbek75 <nodirbek7077@gmail.com>
2025-02-18 21:06:36 +0500

    update Rewards screen UI

--- src/screens/Rewards.tsx
+++ src/screens/Rewards.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
-import * as Animatable from 'react-native-animatable';
+import React, {useEffect} from 'react';
+import {Dimensions} from 'react-native';
 import styled from 'styled-components/native';
-import {Icon} from '@rneui/themed';
+import * as Animatable from 'react-native-animatable';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 // hooks
-import {useRewards} from '../hooks';
+import {useFlashcard} from '../hooks';
+import {useAppSelector} from '../store/hooks';
+import {useNavigation} from '@react-navigation/native';
 
 // assets
-import FlashCard from '../assets/images/flashCard.svg';
+import Pos from '../assets/icons/pos.svg';
+
+const width = Dimensions.get('screen').width;
+
+type Props = StackNavigationProp<RootStackType, 'Home'>;
 
 const Rewards = () => {
-  useRewards();
+  const navigation = useNavigation<Props>();
+
+  const {lnurl, satAmount, displayAmount} = useFlashcard();
+
+  const {currency} = useAppSelector(state => state.amount);
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('RewardsSuccess', {
+        satAmount: 21,
+        displayAmount: 4,
+        balance: `${currency.symbol} ${123}`,
+      });
+    }, 1000);
+  }, []);
 
   return (
     <Wrapper>
-      <Title>Tap your card to get rewards!</Title>
+      <Title>Tap any Flashcard to receive rewards!</Title>
       <Animatable.View
         animation="pulse"
         easing="ease-out"
         iterationCount="infinite">
-        <Icon
-          name={'radio-outline'}
-          size={150}
-          type="ionicon"
-          color={'#41AC48'}
-        />
+        <Image source={Pos} />
       </Animatable.View>
-      <Image source={FlashCard} />
+      <Subtitle>{`21 sats (~J$ 3.19)\nwill be applied to reward balance`}</Subtitle>
     </Wrapper>
   );
 };
@@ -35,17 +51,27 @@ export default Rewards;
 
 const Wrapper = styled.View`
   flex: 1;
-  background-color: #ffffff;
   align-items: center;
+  background-color: #ffffff;
+  padding-top: 100px;
+  padding-horizontal: 40px;
 `;
 
 const Title = styled.Text`
-  font-size: 26px;
+  font-size: 30px;
   font-family: 'Outfit-Medium';
   text-align: center;
-  color: #002118;
-  margin-top: 100px;
-  margin-bottom: 30px;
+  color: #000000;
 `;
 
-const Image = styled.Image``;
+const Image = styled.Image`
+  width: ${width - 80}px;
+  height: ${width - 80}px;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 24px;
+  font-family: 'Outfit-Regular';
+  text-align: center;
+  color: #747474;
+`;
