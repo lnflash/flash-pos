9fb629843497f2166bc0c42270bbb60e46078170
nodirbek75 <nodirbek7077@gmail.com>
2025-02-24 14:37:27 +0500

    update env.d.ts and route.d.ts

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,4 +3,6 @@ declare module '@env' {
   export const FLASH_GRAPHQL_WS_URI = string;
   export const FLASH_LN_ADDRESS_URL = string;
   export const FLASH_LN_ADDRESS = string;
+  export const BTC_PAY_SERVER = string;
+  export const PULL_PAYMENT_ID = string;
 }
--- src/types/routes.d.ts
+++ src/types/routes.d.ts
@@ -3,5 +3,5 @@ type RootStackType = {
   Home: undefined;
   Invoice: undefined;
   Success?: {title?: string};
-  RewardsSuccess: {satAmount: number; displayAmount: number; balance: string};
+  RewardsSuccess: {rewardSatAmount: number; balance: string};
 };
